80b0cdf508150ba9c25507f227c55849efad1b28
Bruna de Carvalho <brunacarvalho260697@gmail.com>
2022-08-06 12:30:16 -0300

    Resolução dos exercicios do Desafio Intermediário Code Girls

--- src/Eleitores.java
+++ src/Eleitores.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class Eleitores {
+import java.util.Scanner;
+
+
+public class Eleitores {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int habitantes = input.nextInt();
+        int brancos = input.nextInt();
+        int nulos = input.nextInt();
+        int validos = input.nextInt();
+
+
+        int pBrancos = (brancos * 100) / habitantes;
+        int pNulos = (nulos * 100)/ habitantes;
+        int pValidos = (validos * 100) / habitantes;
+
+
+        System.out.println("Brancos: " + pBrancos+ "%");
+
+        System.out.println("Nulos: " + pNulos+ "%");
+
+        System.out.println("Validos: " + pValidos+ "%");
+    }
 }
